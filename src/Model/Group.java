c4cd9da9ae6683c958d492af950f2de1fc15b570
Georgiy Satosov <gsatosov@gmail.com>
2017-03-03 22:32:50 +0300

    Moved created files to Model

--- src/Model/Group.java
+++ src/Model/Group.java
@@ -1,10 +1,10 @@
-package Controller;
+package Model;
 
 import java.util.ArrayList;
 
 /**
  * Created by GSatosov on 3/3/2017.
  */
-class Task {
-    private ArrayList<Test> Tests;
+class Group {
+    private ArrayList<Student> Students;
 }
--- src/Model/Task.java
+++ src/Model/Task.java
@@ -1,10 +1,10 @@
-package Controller;
+package Model;
 
 import java.util.ArrayList;
 
 /**
  * Created by GSatosov on 3/3/2017.
  */
-class Group {
-    private ArrayList<Student> Students;
+class Task {
+    private ArrayList<Test> Tests;
 }
