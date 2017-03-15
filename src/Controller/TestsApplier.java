c83157f91342415c639d43838ed3c7be41e82fc6
Georgiy Satosov <gsatosov@gmail.com>
2017-03-16 00:06:54 +0300

    Now exceptions is a valid input for a test

--- src/Controller/TestsApplier.java
+++ src/Controller/TestsApplier.java
@@ -11,23 +11,11 @@ import java.util.HashMap;
  */
 class TestsApplier {
     private volatile boolean notInterrupted;
+    private ArrayList<String> output = new ArrayList<>();
 
-    String applyTests(Task task) throws IOException, InterruptedException {
-        ArrayList<String> output = new ArrayList<>();
-        HashMap<String, ArrayList<String>> testContents = task.getTestContents();
-        char[] functionToTest = task.getName().split("\\.")[0].toCharArray();
-        System.out.println(task.getSourcePath());
-        functionToTest[0] = Character.toLowerCase(functionToTest[0]);
-        ProcessBuilder builder = new ProcessBuilder("ghci");
-        builder.redirectErrorStream(true);
-        Process p = builder.start();
-        PrintStream cmdInput = new PrintStream(p.getOutputStream());
-        InputStream cmdOutput = p.getInputStream();
-        cmdInput.println(":l " + task.getSourcePath());
-        cmdInput.flush();
-        notInterrupted = true;
-        Thread thread = new Thread(() -> {
-            try (BufferedReader r = new BufferedReader(new InputStreamReader(cmdOutput))) {
+    private Thread cmdOutput(InputStream stream) {
+        return new Thread(() -> {
+            try (BufferedReader r = new BufferedReader(new InputStreamReader(stream))) {
                 String ch;
                 while (notInterrupted) {
                     ch = r.readLine();
@@ -38,7 +26,24 @@ class TestsApplier {
                 System.out.println(e.getMessage());
             }
         });
-        thread.start();
+    }
+
+    String applyTests(Task task) throws IOException, InterruptedException {
+        output.clear();
+        HashMap<String, ArrayList<String>> testContents = task.getTestContents();
+        char[] functionToTest = task.getName().split("\\.")[0].toCharArray();
+        System.out.println(task.getSourcePath());
+        functionToTest[0] = Character.toLowerCase(functionToTest[0]);
+        ProcessBuilder builder = new ProcessBuilder("ghci");
+        builder.redirectErrorStream(true);
+        Process p = builder.start();
+        PrintStream cmdInput = new PrintStream(p.getOutputStream());
+        InputStream cmdOutputStream = p.getInputStream();
+        cmdInput.println(":l " + task.getSourcePath());
+        cmdInput.flush();
+        notInterrupted = true;
+        Thread cmdOutputThread = cmdOutput(cmdOutputStream);
+        cmdOutputThread.start();
         int maxScore = testContents.size();
         for (int i = 0; i < 6; i++) {
             if (i == 5) return "TL"; //Time Limit
@@ -47,15 +52,15 @@ class TestsApplier {
             Thread.sleep(500);
         }
         long testingScore = testContents.entrySet().stream().filter(a -> {
-            int beforeCommand = output.size();
+            int beforeTesting = output.size();
             String testInput = a.getKey();
-            ArrayList<String> taskOutputs = a.getValue();
+            ArrayList<String> testOutputVariants = a.getValue();
             cmdInput.println(new String(functionToTest) + " " + testInput);
             cmdInput.flush();
             int time = 0;
             while (true) {
                 time++;
-                if (output.size() > beforeCommand) break;
+                if (output.size() > beforeTesting) break;
                 if (time == 200) return false; //Took too long to compute.
                 try {
                     Thread.sleep(10);
@@ -63,12 +68,13 @@ class TestsApplier {
                     e.printStackTrace();
                 }
             }
-            String response = output.get(output.size() - 1).split(" ", 2)[1]; // *>TaskName> Output
-            return taskOutputs.contains(response);
+            String response = output.get(beforeTesting).split(" ", 2)[1]; // *>TaskName> Output
+            return testOutputVariants.contains("Error") && output.get(beforeTesting).split(" ", 2)[1].startsWith("*** Exception") // If exception is expected.
+                    || testOutputVariants.contains(response);
         }).count();
         cmdInput.close();
         notInterrupted = false;
-        thread.interrupt();
+        cmdOutputThread.interrupt();
         output.forEach(System.out::println);
         return testingScore + "/" + maxScore;
     }
