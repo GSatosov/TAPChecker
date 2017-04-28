996738f1120bfc54ddb692d6d9b8bbb212cc0017
nazarov <arsnazarov94@gmail.com>
2017-04-28 04:15:39 +0300

    Login window

--- src/View/LoginController.java
+++ src/View/LoginController.java
@@ -1,11 +1,11 @@
 package View;
 
 import Model.Settings;
+import javafx.collections.FXCollections;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Button;
-import javafx.scene.control.PasswordField;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
+import javafx.scene.layout.GridPane;
 
 import javax.crypto.NoSuchPaddingException;
 import java.io.IOException;
@@ -22,20 +22,26 @@ public class LoginController implements Initializable {
     private TextField emailField;
     @FXML
     private PasswordField pwField;
+    @FXML
+    private ChoiceBox<String> mailServer;
 
     @FXML
     private Button login;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        mailServer.setItems(FXCollections.observableArrayList("@gmail.com", "@mail.ru"));
+        mailServer.setValue(mailServer.getItems().get(0));
+
         login.setOnAction(event ->  {
-            Settings.getInstance().setEmail(emailField.getText());
+            Settings.getInstance().setEmail(emailField.getText() + mailServer.getValue());
             Settings.getInstance().setPassword(pwField.getText());
             try {
                 Settings.getInstance().saveSettings();
             } catch (InvalidKeyException | NoSuchAlgorithmException | NoSuchPaddingException | IOException e) {
                 e.printStackTrace();
             }
+            pwField.clear();
             MainFrame.getPrimaryStage().setScene(MainFrame.getMainScene());
         });
     }
